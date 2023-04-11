beca7be0ec7959cc4188765a2c0bbde4ea4c26b9
Frederik Wessel <frederikglimsager@hotmail.com>
2023-04-11 15:17:18 +0200

    tilføjet CRUD

--- onskeliste/src/main/java/com/example/onskeliste/controller/HomeController.java
+++ onskeliste/src/main/java/com/example/onskeliste/controller/HomeController.java
@@ -1,15 +1,84 @@
 package com.example.onskeliste.controller;
 
+import com.example.onskeliste.model.Product;
+import com.example.onskeliste.repository.ProductRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
 
+    private ProductRepository productRepository;
+
+    public HomeController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
     @GetMapping("/")
     public String index(Model model) {
+        model.addAttribute("products", productRepository.getAll());
         return "index";
     }
 
+    //fra anchor i index
+    @GetMapping("/create")
+    public String showCreate() {
+        //vis create-siden
+        return "create";
+    }
+
+    //fra form action
+    @PostMapping("/create")
+    public String createProduct(@RequestParam("product-name") String newName,
+                                @RequestParam("product-price") double newPrice) {
+        //lave et nyt Product
+        Product newProduct = new Product();
+        newProduct.setPrice(newPrice);
+        newProduct.setName(newName);
+
+        //gem nyt produkt
+        productRepository.addProduct(newProduct);
+
+        //tilbage til produktlisten
+        return "redirect:/";
+    }
+
+    @GetMapping("/update/{id}")
+    public String showUpdate(@PathVariable("id") int updateId, Model model) {
+        //find produkt med id=updateId i databasen
+        Product updateProduct = productRepository.findProductById(updateId);
+
+        //tilføj produkt til viewmodel, så det kan bruges i Thymeleaf
+        model.addAttribute("product", updateProduct);
+
+        //fortæl Spring hvilken HTML-side, der skal vises
+        return "update";
+    }
+
+    @PostMapping("/update")
+    public String updateProduct(@RequestParam("product-name") String updateName,
+                                @RequestParam("product-price") double updatePrice,
+                                @RequestParam("product-id") int updateId) {
+        //lav produkt ud fra parametre
+        Product updateProduct = new Product(updateId, updateName, updatePrice);
+
+        //kald opdater i repository
+        productRepository.updateProduct(updateProduct);
+
+        //rediriger til oversigtssiden
+        return "redirect:/";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteProduct(@PathVariable("id") int id) {
+        //slet fra repository
+        productRepository.deleteById(id);
+
+        //returner til index-siden
+        return "redirect:/";
+    }
 }
--- onskeliste/src/main/java/com/example/onskeliste/repository/ProductRepository.java
+++ onskeliste/src/main/java/com/example/onskeliste/repository/ProductRepository.java
@@ -5,10 +5,7 @@ import com.example.onskeliste.utility.ConnectionManager;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -48,5 +45,106 @@ public class ProductRepository {
         return productList;
     }
 
+    public void addProduct(Product product){
+        try{
+            //connect to db
+            Connection connection = ConnectionManager.getConnection(DB_URL, UID, PWD);
+            final String CREATE_QUERY = "INSERT INTO products(name, price) VALUES  (?, ?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(CREATE_QUERY);
+
+            //set attributer i prepared statement
+            preparedStatement.setString(1, product.getName());
+            preparedStatement.setDouble(2, product.getPrice());
+
+            //execute statement
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Could not create product");
+            e.printStackTrace();
+        }
+    }
+
+    public void updateProduct(Product product){
+        //SQL statement
+        final String UPDATE_QUERY = "UPDATE products SET name = ?, price = ? WHERE id = ?";
+
+        try {
+            //connect db
+            Connection connection = ConnectionManager.getConnection(DB_URL, UID, PWD);
+
+            //prepared statement
+            PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_QUERY);
+
+            //set parameters
+            String name = product.getName();
+            double price = product.getPrice();
+            int id = product.getId();
+            preparedStatement.setString(1, name);
+            preparedStatement.setDouble(2, price);
+            preparedStatement.setInt(3, id);
+
+            //execute statement
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Could not update product");
+            e.printStackTrace();
+        }
+    }
+
+    public Product findProductById(int id){
+        //SQL-statement
+        final String FIND_QUERY = "SELECT * FROM products WHERE id = ?";
+        Product product = new Product();
+        product.setId(id);
+        try {
+            //db connection
+            Connection connection = ConnectionManager.getConnection(DB_URL, UID, PWD);
+
+            //prepared statement
+            PreparedStatement preparedStatement = connection.prepareStatement(FIND_QUERY);
+
+            //set parameters
+            preparedStatement.setInt(1, id);
+
+            //execute statement
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            //få product ud af resultset
+            resultSet.next();
+            String name = resultSet.getString(2);
+            double price = resultSet.getDouble(3);
+            product.setName(name);
+            product.setPrice(price);
+        } catch (SQLException e){
+            System.out.println("Could not find product");
+            e.printStackTrace();
+        }
+        System.out.println(product);
+        //return product
+        return product;
+    }
+
+    public void deleteById(int id){
+        //SQL-query
+        final String DELETE_QUERY = "DELETE FROM products WHERE id=?";
+
+        try {
+            //connect til db
+            Connection connection = ConnectionManager.getConnection(DB_URL, UID, PWD);
+
+            //create statement
+            PreparedStatement preparedStatement = connection.prepareStatement(DELETE_QUERY);
+
+            //set parameter
+            preparedStatement.setInt(1, id);
+
+            //execute statement
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e){
+            System.out.println("Could not delete product");
+            e.printStackTrace();
+        }
+    }
 
 }

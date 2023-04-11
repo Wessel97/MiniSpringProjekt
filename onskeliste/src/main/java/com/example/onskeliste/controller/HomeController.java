07129120be4961054a728e526296a4d5f07d9d6d
Frederik Wessel <frederikglimsager@hotmail.com>
2023-04-11 14:31:35 +0200

    tilføjet template kode

--- onskeliste/src/main/java/com/example/onskeliste/controller/HomeController.java
+++ onskeliste/src/main/java/com/example/onskeliste/controller/HomeController.java
@@ -1,4 +1,23 @@
 package com.example.onskeliste.controller;
 
-public class HomeController {
+import com.example.onskeliste.model.Product;
+import com.example.onskeliste.repository.ProductRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController
+{
+    private ProductRepository productRepository;
+    public HomeController(ProductRepository productRepository){
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping("/")
+    public String index(Model model){
+        model.addAttribute("products", productRepository.getAll());
+        return "index";
+    }
+
 }
--- onskeliste/src/main/java/com/example/onskeliste/repository/ProductRepository.java
+++ onskeliste/src/main/java/com/example/onskeliste/repository/ProductRepository.java
@@ -1,4 +1,44 @@
 package com.example.onskeliste.repository;
 
+import com.example.onskeliste.model.Product;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProductRepository {
+
+    @Value("${spring.datasource.url}")
+    private String DB_URL;
+
+    @Value("${spring.datasource.username}")
+    private String UID;
+
+    @Value("${spring.datasource.password}")
+    private String PWD;
+
+    public List<Product> getAll(){
+        List<Product> productList = new ArrayList<>();
+        try {
+            Connection connection = DriverManager.getConnection(DB_URL, UID, PWD);
+            Statement statement = connection.createStatement();
+            final String SQL_QUERY = "SELECT * FROM products";
+            ResultSet resultSet = statement.executeQuery(SQL_QUERY);
+            while (resultSet.next()){
+                int id = resultSet.getInt(1);
+                String name = resultSet.getString(2);
+                double price = resultSet.getDouble(3);
+                Product product = new Product(id, name, price);
+                productList.add(product);
+                System.out.println(product);
+            }
+        }
+        catch(SQLException e)
+        {
+            System.out.println("Could not query database");
+            e.printStackTrace();
+        }
+        return productList;
+    }
 }

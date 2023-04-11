85678091143d16a3837fe11e9e41de5dfd76176f
Frederik Wessel <frederikglimsager@hotmail.com>
2023-04-11 15:13:02 +0200

    tilføjet lidt Jarl kode

---
+++ onskeliste/src/main/java/com/example/onskeliste/repository/ProductRepository.java
@@ -0,0 +1,52 @@
+package com.example.onskeliste.repository;
+
+import com.example.onskeliste.model.Product;
+import com.example.onskeliste.utility.ConnectionManager;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class ProductRepository {
+
+    @Value("${spring.datasource.url}")
+    private String DB_URL;
+
+    @Value("${spring.datasource.username}")
+    private String UID;
+
+    @Value("${spring.datasource.password}")
+    private String PWD;
+
+    public List<Product> getAll(){
+        List<Product> productList = new ArrayList<>();
+        try {
+            Connection connection = ConnectionManager.getConnection(DB_URL, UID, PWD);
+            Statement statement = connection.createStatement();
+            final String SQL_QUERY = "SELECT * FROM products";
+            ResultSet resultSet = statement.executeQuery(SQL_QUERY);
+            while (resultSet.next()){
+                int id = resultSet.getInt(1);
+                String name = resultSet.getString(2);
+                double price = resultSet.getDouble(3);
+                Product product = new Product(id, name, price);
+                productList.add(product);
+                System.out.println(product);
+            }
+        }
+        catch(SQLException e)
+        {
+            System.out.println("Could not query database");
+            e.printStackTrace();
+        }
+        return productList;
+    }
+
+
+}
